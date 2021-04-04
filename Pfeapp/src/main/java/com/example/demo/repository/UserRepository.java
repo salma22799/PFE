f281a0e8adb7eb8302a158856eb2e08d7652a0dd
salma <74369762+salma22799@users.noreply.github.com>
2021-04-04 13:25:46 +0100

    frontend

--- Pfeapp/src/main/java/com/example/demo/repository/UserRepository.java
+++ Pfeapp/src/main/java/com/example/demo/repository/UserRepository.java
@@ -8,7 +8,6 @@ import com.example.demo.model.User;
 @Repository 
 public interface UserRepository extends JpaRepository <User ,Long>{
     
-    hhhh
 
 	
 }
---
+++ Pfefrontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/nav/navbar/navbar.component';
+import { AuthentificationComponent } from './components/authentification/authentification/authentification.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    AuthentificationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
